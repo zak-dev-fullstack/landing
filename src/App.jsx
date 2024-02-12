9693ad3d3c7cf6ccf409e2c6fbffc7bf60c1323c
zak-dev-fullstack <zak.dev.fullstack@proton.me>
2024-02-11 20:45:54 -0600

    a1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Landing Web </h1>
+    <h2>Web Ready for Idea or Project</h2>
+    
+    </>
+  )
+}
+
+export default App
